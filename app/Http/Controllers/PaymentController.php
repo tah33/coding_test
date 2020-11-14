0f4451480a516cc9f2359ff5e9ba8fcbc367d133
Tanvir <spn15@spondonit.com>
2020-11-14 15:40:32 +0600

    doing functionality for completing payment process

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,11 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Stripe\Charge;
+use Stripe\Stripe;
 
 class PaymentController extends Controller
 {
     public function activate()
     {
         return view('payment.activate');
+    }
+
+    public function payment(Request $request)
+    {
+        Stripe::setApiKey('sk_test_lLNB8XEqosKY4wou5vXYCftV00wWpnPhOq');
+
+        Charge::create([
+           'amount' => 100*10,
+           'currency' => 'usd',
+           'description' => 'Monthly Subscription',
+           'source' => $request->stripeToken,
+        ]);
+
+        return redirect()->route('home')->with('success','Congrats! Your Monthly Subscription Has Been Done');
     }
 }
--- resources/views/payment/activate.blade.php
+++ resources/views/payment/activate.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12 col-lg-12 col-sm-12 col-12">
-                <p>You will be charged for $10 for thim monthly subscription</p>
+                <p>You will be charged for $10 for this monthly subscription</p>
                 <div class="card">
                     <form action="{{route('stripe.payment')}}" method="post" id="payment-form">@csrf
                         <div class="form-group">
@@ -15,7 +15,6 @@
 
                                 </div>
                                 <div id="card-errors" role="alert"></div>
-                                <input type="hidden" name="user_id" value="{{\Illuminate\Support\Facades\Auth::id()}}">
                             </div>
                         </div>
                         <div class="card-footer">
@@ -27,3 +26,85 @@
         </div>
     </div>
 @stop
+@section('scripts')
+    <script src="https://js.stripe.com/v3/"></script>
+    <script type="text/javascript">
+
+        //stripe publishable key
+        var stripe = Stripe('pk_test_hXUZ4DOz6Bx5CdBWey9wfv7r002EPTSrbN');
+
+        // creating insatnce of stripe
+        var elements = stripe.elements();
+
+        // custom styling for better design
+        var style = {
+            base: {
+                color: '#32325d',
+                lineHeight: '18px',
+                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                fontSmoothing: 'antialiased',
+                fontSize: '16px',
+                '::placeholder': {
+                    color: '#aab7c4'
+                }
+            },
+            invalid: {
+                color: '#fa755a',
+                iconColor: '#fa755a'
+            }
+        };
+
+        // creating instance for card
+        var card = elements.create('card',{style:style});
+
+        // Add an instance of card
+        card.mount('#card-element');
+
+        // Handling validations
+        card.addEventListener('change',function (event){
+            var displayerror = document.getElementById('card-errors');
+
+            if (event.error)
+            {
+                displayerror.textContent = event.error.message;
+            }
+            else {
+                displayerror.textContent = '';
+            }
+        });
+
+        //handling form submissions
+        var form = document.getElementById('payment-form');
+        form.addEventListener('submit',function (event){
+            event.preventDefault();
+
+            stripe.createToken(card).then(function (result){
+                if (result.error)
+                {
+                    var errorElement = document.getElementById('card-errors');
+                    errorElement.textContent = result.error.message;
+                }
+                else{
+                    //send token to the server
+                    stripeTokenHandler(result.token);
+                }
+            });
+        });
+
+        //getting stripe token for source;
+        function stripeTokenHandler(token)
+        {
+            var form = document.getElementById('payment-form');
+            var hiddenInput = document.createElement('input');
+            hiddenInput.setAttribute('type','hidden');
+            hiddenInput.setAttribute('name','stripeToken');
+            hiddenInput.setAttribute('value',token.id);
+            form.appendChild(hiddenInput);
+
+            //Submit the form
+            form.submit();
+        }
+
+
+    </script>
+@endsection
